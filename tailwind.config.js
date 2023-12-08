776aebce6c8ba42bbe030f6d541dc5a799ea3f71
Vladimir Blake <mcpebyjohan@gmail.com>
2023-12-08 23:49:54 +0300

    feat: configure tailwind and add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+    colors: {
+      black: "#252525",
+      green: "#178080",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      objectSans: ["Object Sans", "sans-serif"],
+      darco: ["Darco", "sans-serif"],
     },
   },
   plugins: [],
-}
+};
