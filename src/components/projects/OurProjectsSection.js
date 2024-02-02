d30ef63b4206146615ef9e0716a618c88b6f5c71
Vladimir Blake <mcpebyjohan@gmail.com>
2024-02-02 18:11:23 +0300

    feat(services, project): add remaining blueprints

--- src/components/projects/OurProjectsSection.js
+++ src/components/projects/OurProjectsSection.js
@@ -2,12 +2,24 @@
 
 import { Carousel } from "flowbite-react";
 import Image from "next/image";
+import Blueprint from "@/images/projects/blueprint.png";
+import Blueprint2 from "@/images/projects/blueprint2.png";
 
 const projects = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 export default function OurProjectsSection() {
   return (
-    <section className="pt-16 xl:pt-20">
+    <section className="relative pt-16 xl:pt-20">
+      <Image
+        src={Blueprint}
+        alt=""
+        className="absolute hidden xl:block -z-10 right-0 top-0"
+      />
+      <Image
+        src={Blueprint2}
+        alt=""
+        className="absolute hidden xl:block -z-10 left-0 top-12"
+      />
       <h2 className="text-center font-darco text-2xl mb-5 font-bold xl:text-4xl xl:mb-12">
         Наши проекты
       </h2>
--- src/components/services/ExtraServicesSection.js
+++ src/components/services/ExtraServicesSection.js
@@ -1,10 +1,16 @@
 import Image from "next/image";
 import Service1 from "../../../public/service-extra-1.png";
 import Service2 from "../../../public/service-extra-2.png";
+import Blueprint from "@/images/services/blueprint2.png";
 
 export default function ExtraServicesSection() {
   return (
-    <section className="xl:container xl:mx-auto xl:grid xl:grid-cols-2 xl:gap-x-6 xl:grid-rows-[438px] xl:mt-16">
+    <section className="relative xl:container xl:mx-auto xl:grid xl:grid-cols-2 xl:gap-x-6 xl:grid-rows-[438px] xl:mt-16">
+      <Image
+        src={Blueprint}
+        alt=""
+        className="hidden xl:block absolute -z-10 -left-20 bottom-8"
+      />
       <div className="sm:container sm:mx-auto xl:bg-serviceExtra1 xl:bg-cover xl:bg-no-repeat xl:rounded-3xl xl:p-7">
         <h2 className="mt-12 px-4 sm:px-0 font-darco font-bold text-xl xl:mt-0 xl:text-white">
           Выполнение авторского надзора за строительством по проектной и рабочей
